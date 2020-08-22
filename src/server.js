23e7f3dbc4cd8f89bed9dd1909469f8a8b40fa59
Luís Clício <cliciosa14@gmail.com>
2020-08-21 21:58:21 -0300

    feat: Configure database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,40 @@
 require('dotenv-save').config();
+
+const db = require('./config/database');
 const app = require('./app');
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server running in http://localhost:${PORT}...`);
-});
+(async () => {
+  await db.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id VARCHAR (8),
+      username VARCHAR (255) NOT NULL UNIQUE,
+      email VARCHAR (255) NOT NULL UNIQUE,
+      password TEXT NOT NULL,
+      PRIMARY KEY (id)
+    );
+  `, []);
+
+  await db.query(`
+    CREATE TABLE IF NOT EXISTS users_todos (
+      id VARCHAR (8),
+      user_id VARCHAR (8) NOT NULL,
+      title TEXT NOT NULL,
+      description TEXT,
+      priority SMALLINT NOT NULL
+        CHECK (priority > 0 AND priority < 4),
+      finished BOOLEAN DEFAULT FALSE,
+      data_added TEXT NOT NULL,
+      PRIMARY KEY (id),
+      CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+          REFERENCES users (id)
+          ON DELETE CASCADE
+    );
+  `, []);
+
+  app.listen(PORT, () => {
+    console.log(`Server running in http://localhost:${PORT}...`);
+  });
+})();
