b9c5f0e201fa079b28114ac0eedd221940a698e1
Luís Clício <cliciosa14@gmail.com>
2020-08-24 16:14:41 -0300

    feat: Add 'delete to do end point'

--- src/controllers/user/deleteToDo.js
+++ src/controllers/user/deleteToDo.js
@@ -1,1 +1,34 @@
-module.exports = (req, res) => { }
+const db = require('../../config/database');
+
+module.exports = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const { rows: todosRows } = await db.query(`
+      SELECT id FROM users_todos WHERE id = '${id}' LIMIT 1;
+    `, []);
+
+    if (todosRows.length === 0)
+      return res.status(404).json({
+        error: true,
+        message: 'Could not delete todo: doesn\'t exist.'
+      });
+
+    await db.query(`
+      DELETE FROM users_todos WHERE id = '${id}';
+    `, []);
+
+    return res.status(200).json({
+      error: false,
+      deleted_todo: {
+        id: id
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: true,
+      message: 'Could not delete todo.'
+    });
+  }
+}
