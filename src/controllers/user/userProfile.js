e3349c1af56c8bba20d20d211171dcb8fad17719
Luís Clício <cliciosa14@gmail.com>
2020-08-26 15:01:03 -0300

    refactor: Refactor 'userProfile.js'.

--- src/controllers/user/userProfile.js
+++ src/controllers/user/userProfile.js
@@ -3,20 +3,19 @@ const db = require('../../config/database');
 module.exports = async (req, res) => {
   const { userId } = req;
 
-  const user = {
-    username: null,
-    email: null,
-  }
-
   try {
-    const userResult = await db.query(`
-      SELECT username, email FROM users WHERE id = '${userId}';
-    `, []);
+    const { rows: [{ username, email }] } = await db.query(`
+      SELECT username, email
+      FROM users
+      WHERE id = $1;
+    `, [userId]);
 
-    const { username, email } = userResult.rows[0];
-
-    user.username = username;
-    user.email = email;
+    return res.status(200).json({
+      error: false,
+      auth: true,
+      user_name: username,
+      user_email: email
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -25,11 +24,4 @@ module.exports = async (req, res) => {
       message: 'Could not get user data.'
     });
   }
-
-  return res.status(200).json({
-    error: false,
-    auth: true,
-    user_name: user.username,
-    user_email: user.email,
-  });
 }
