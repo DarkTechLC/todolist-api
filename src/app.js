14db16d5e8b8e701e77aec358c9f77e505999bb8
Luís Clício <cliciosa14@gmail.com>
2020-08-29 10:45:48 -0300

    chore: Add 'CORS package'

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const userRoutes = require('./routes/user');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
